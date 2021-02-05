f35e5ab63f090dd9d3428c5bf7aa3cc1a888d888
Hagen DeRouen <hagend87@gmail.com>
2021-02-05 12:04:29 -0600

    Add create password and get random character functions. Update get random number function to include min and max parameters included in range. Update generate password function to return a created password. Removed pseudo code. Updated comments to reflect changes.

--- script.js
+++ script.js
@@ -14,22 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  // prompt user for password length between 8 and 128 chars.
   var passwordLength = getUserPasswordLength();
-  console.log(`Password length: ${passwordLength}`);
-  // prompt user for password combinations (lowercase, uppercase, numeric, and/or special characters).
   var passwordCombos = getUserPasswordCombos();
-  console.log("Password combos:");
-  console.log(passwordCombos);
-  // return password combination
-  return "Password#1" // ! for testing purposes
+
+  return createPassword(passwordLength, passwordCombos);
 }
 
 // Prompts user for password length with specified min and max characters.
 function getUserPasswordLength() {
   var passwordLengthInput = prompt("Specify password length betwen 8 and 128 characters.", "16");
   
-  while (!passwordLengthIsValid(passwordLengthInput, 8, 128)) {
+  while (!lengthIsValid(passwordLengthInput, 8, 128)) {
     passwordLengthInput = prompt("Try again. Password length must be between 8 and 128 characters.");
   }
 
@@ -37,7 +32,7 @@ function getUserPasswordLength() {
   
 }
 
-// Prompts user of password combinations. Checks user input and returns array based on input.
+// Prompts user of password combinations. Returns an array of user specified character types.
 function getUserPasswordCombos() {
   var combosInput = prompt("Specify use of password combinations", "lowercase, uppercase, numeric, or special");
   var combos = [];
@@ -68,16 +63,66 @@ function getUserPasswordCombos() {
 
 }
 
+// Creates a password from numeric length and combos array. Defaults lower and uppercase chars if combos is empty.
+function createPassword(charLength, combosArray = ["lowercase", "uppercase"]) {
+  var password = "";
+
+  for (var i = 0; i < charLength; i++) {
+    var charType = combosArray[getRandomNumber(0, combosArray.length)];
+    var randChar = getRandomChar(charType);
+
+    password += randChar;
+  }
+
+  return password;
+}
+
+// Gets a random character from charType (lowercase, uppercase, special, numeric)
+function getRandomChar(charType) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArr = alphabet.split('');
+  const specialChars = "~`!@#$%^&*()-+={[}]|:;'<,>.?/_";
+  const specialCharsArr = specialChars.split('');
+  var randChar;
+
+  switch(charType) {
+    case "lowercase":
+      var rand = getRandomNumber(0, 26);
+      randChar = alphabetArr[rand].toLowerCase();
+      break;
+    case "uppercase":
+      var rand = getRandomNumber(0, 26);
+      randChar = alphabetArr[rand].toUpperCase();
+      break;
+    case "special":
+      var rand = getRandomNumber(0, 30);
+      randChar = specialCharsArr[rand];
+      break;
+    case "numeric":
+      randChar = getRandomNumber(0, 9);
+      break;
+    default:
+      var rand = getRandomNumber(0, 26);
+      randChar = alphabet[rand].toLowerCase();
+  }
+
+  return randChar;
+}
+
 // * Utility Functions
 // Checks if an input is between min and max values 
-function passwordLengthIsValid(input, min, max) {
-  if (!input) { // if the input is empty or null
+function lengthIsValid(input, min, max) {
+  if (!input) { 
     return false;
   } else if (input >= min && input <= max) {
     return true;
   } else {
     return false;
   }  
+}
+// Returns random number in range included
+function getRandomNumber(min, max) {
+  return Math.floor((Math.random() * max) + min);
 }
 
 // *
